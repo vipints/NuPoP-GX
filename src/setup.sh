9b649a3e6a3e24ffe3f0a5c2a13d55a159d451bf
vipints <vipin@cbio.mskcc.org>
2014-03-17 22:59:22 -0400

    setup program

---
+++ src/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+##
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Copyright (C) 2014 Memorial Sloan Kettering Cancer Center
+##
+
+set -e
+
+DIR=`dirname $0`
+
